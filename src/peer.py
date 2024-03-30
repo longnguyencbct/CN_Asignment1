60da83f3dd0749f9e6b1224a86471bb750f3e2f1
cbct <clongnguynvn@gmail.com>
2024-03-30 16:30:25 +0700

    added update_peer_set

--- src/peer.py
+++ src/peer.py
@@ -1,8 +1,33 @@
-class Peer:
+from bencode import bencode,bdecode
+class Peer_Info:
     def __init__(self,peer_id="",ip="",port=9999):
         self.peer_id=peer_id
         self.ip=ip
         self.port=port
+        self.downloaded=0
+        self.uploaded=0
+    def Bencode(self):
+        return bencode({
+            "peer_id":self.peer_id,
+            "ip":self.ip,
+            "port":self.port,
+            "downloaded":self.downloaded,
+            "uploaded":self.uploaded
+        })
+class Peer:
+    def __init__(self,peer_info=Peer_Info(),peer_data=None,seeder_lst=[],leecher_lst=[]):
+        self.peer_info=peer_info
+        self.peer_data=peer_data
+        self.seeder_lst=seeder_lst
+        self.leacher_lst=leecher_lst
     def request_tracker(self):
+        # request tracker to get seeder_lst
         pass
+    def request_download(self):
+        # request seeders in seeder_lst to download piece
+        pass
+    def upload_response(self):
+        # upload file to leachers in leecher_lst
+        pass
+
     
--- src/tracker.py
+++ src/tracker.py
@@ -1,31 +1,6 @@
 from time import sleep
 import threading
-
-class response_struct:
-    def __init__(self, tracker_id=9999,
-                 peer_set=[
-                     {
-                         "peer_id": 1234,     # peer's self-selected ID
-                         "ip": "DNS...",      # peer's IP address (DNS name)
-                         "port": 5678         # peer's port number
-                     },
-                     {
-                         "peer_id": 9341,
-                         "ip": "DNS...",
-                         "port": 8269
-                     }
-                 ]):
-        self.tracker_id = tracker_id
-        self.peer_set = peer_set
-
-    def update_peer_set(self):
-        pass
-
-class request_struct:
-    def __init__(self, magnet_text, request_state="STARTED", bytes_downloaded=0):
-        self.magnet_text = magnet_text
-        self.request_state = request_state
-        self.bytes_downloaded = bytes_downloaded
+from request_response_struct import request_struct,response_struct
 
 class Tracker:
     def __init__(self, currTorrent=response_struct(), request_queue=[]):
@@ -38,7 +13,12 @@ class Tracker:
         temp = 0
         while self.running:
             sleep(1)
+            #####################################################################
             new_request = request_struct(f"Sample magnet link {temp}")
+            # GET NEW REQUEST HERE
+            # TODO
+            # READ FROM CLIENT "request_struck" object
+            #####################################################################
             self.request_queue.append(new_request)
             if self.display_requests:  # Check if display flag is set
                 print("Added request to queue:", new_request.magnet_text)
@@ -48,8 +28,18 @@ class Tracker:
         while self.running:
             while len(self.request_queue) != 0:
                 head_request = self.request_queue.pop(0)
+                #################################################################
+                # RESPOND REQUEST HERE
+                # TODO
+                # ANALYZE HEAD REQUEST
+                # SEND BACK CLIENT A "response_struct" OBJECT
+                #################################################################
                 if self.display_requests:  # Check if display flag is set
                     print("Responding request:", head_request.magnet_text)
+                    peer_set_id=[]
+                    for i in self.currTorrent.peer_set:
+                        peer_set_id.append(i["peer_id"])
+                    print("Current Peer Set ID:", peer_set_id)
             sleep(5)
 
     def handle_user_input(self):
@@ -60,17 +50,24 @@ class Tracker:
                 break  # Break out of the loop to exit the program
             elif command.strip() == "/display":
                 self.display_requests = not self.display_requests  # Toggle the display flag
-
+    def UpdateTorrent(self):
+        while self.running:
+            print("################\nUpdated Peer Set\n################")
+            self.currTorrent.update_peer_set()
+            sleep(20)
     def tracker_main(self):
+        update_Torrent_thread= threading.Thread(target=self.UpdateTorrent)
         request_sniffing_thread = threading.Thread(target=self.request_sniffing)
         tracker_response_thread = threading.Thread(target=self.tracker_response)
         user_input_thread = threading.Thread(target=self.handle_user_input)
 
+        update_Torrent_thread.start()
         request_sniffing_thread.start()
         tracker_response_thread.start()
         user_input_thread.start()
 
         # Wait for threads to finish (which they never will in this example because of the infinite loops)
+        update_Torrent_thread.join()
         request_sniffing_thread.join()
         tracker_response_thread.join()
         user_input_thread.join()
